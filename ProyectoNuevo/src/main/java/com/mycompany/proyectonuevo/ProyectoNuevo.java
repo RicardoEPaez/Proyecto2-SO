14aae7989e6a0720e1a0f399fb86053877630fa2
RicardoEPaez <ricardo.paez@correo.unimet.edu.ve>
2026-03-07 11:47:46 -0400

    Estructura base del Proyecto

---
+++ ProyectoNuevo/src/main/java/com/mycompany/proyectonuevo/ProyectoNuevo.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.proyectonuevo;
+
+/**
+ *
+ * @author ricar
+ */
+public class ProyectoNuevo {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
